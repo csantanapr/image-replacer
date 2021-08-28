13a582c2ca502e43ffbc536bd445de03ffb831f8
Carlos Santana <csantana23@gmail.com>
2021-08-27 21:15:32 -0400

    code-engin
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- nodejs/app/server.js
+++ nodejs/app/server.js
@@ -3,35 +3,37 @@ const https = require('https')
 const http = require('http')
 const fs = require('fs')
 const bodyParser = require('body-parser')
-const yaml = require('js-yaml')
 
 const app = express()
 app.use(bodyParser.json())
 
+const defaultReplacer = '{"ghcr.io/rawkode/klustered:v2":"quay.io/csantanapr/klustered:v2"}'
+const REPLACER = process.env.REPLACER || defaultReplacer
+const imagesToReplace = JSON.parse(REPLACER)
+console.log('Replacing pod images:')
+console.log(JSON.stringify(imagesToReplace, null, 2))
 
-const IMAGE = process.env.IMAGE || 'quay.io/bitnami/nginx'
-
-
-const MESSAGE=`Replacing pod images with ${IMAGE}`
-console.log(MESSAGE)
-app.get('/', (req, res) => res.json({ status: MESSAGE }));
+app.get('/', (req, res) => res.json({ images: imagesToReplace }))
 
 app.post('/', (req, res) => {
   if (req.body.request === undefined || req.body.request.uid === undefined) {
     res.status(400).send()
     return
   }
-
+  const uid = req.body.request.uid
+  const object = req.body.request.object
+  const toPatch = []
+  object.spec.containers.forEach((value, index, arr) => {
+    for (const [original, replace] of Object.entries(imagesToReplace)) {
+      if (value.image === original) {
+        console.log(`replacing container[${index}] ${value.image} with ${replace}`)
+        toPatch.push({ op: 'replace', path: `/spec/containers/${index}/image`, value: replace })
+      }
+    }
+  })
   const allowed = true
   const code = 200
   const message = ''
-  const uid = req.body.request.uid
-  const object = req.body.request.object
-  console.log('#############################################')
-  console.log(JSON.stringify(req.body.request, null, 2)) // debug
-
-  const toPatch = []
-  toPatch.push({ op: 'replace', path: '/spec/containers/0/image', value: IMAGE })
 
   const review = {
     apiVersion: 'admission.k8s.io/v1',
@@ -46,21 +48,20 @@ app.post('/', (req, res) => {
     }
   }
   if (toPatch.length > 0) {
+    console.log(JSON.stringify(toPatch, null, 2)) // debug
     const dataAsString = JSON.stringify(toPatch)
     const buff = Buffer.from(dataAsString.toString(), 'utf8')
     const patch = buff.toString('base64')
     review.response.patchType = 'JSONPatch'
     review.response.patch = patch
   }
-  console.log('response', JSON.stringify(review, null, 2))
   res.send(review)
 })
 
 const port = process.env.PORT || '8443'
-var options = {}
-var server = {}
+let server = {}
 if (port !== '8080') {
-  options = {
+  const options = {
     cert: fs.readFileSync('/certs/tls.crt'),
     key: fs.readFileSync('/certs/tls.key')
   }
@@ -69,9 +70,6 @@ if (port !== '8080') {
   server = http.createServer(app)
 }
 
-
-
-
 server.listen(port, () => {
-  console.log(`mutating controller running on port ${port}/`) // debug
+  console.log(`mutating controller running on port ${port}/`)
 })
